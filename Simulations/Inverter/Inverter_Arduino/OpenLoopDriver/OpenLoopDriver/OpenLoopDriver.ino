177a8b2cc7bb7a408bad43d770c11b5eb6a0bdde
Muhammad Shamaas <mshamaas1997@gmail.com>
2020-04-30 01:23:16 +0500

    V1

--- Simulations/Inverter/Inverter_Arduino/OpenLoopDriver/OpenLoopDriver/OpenLoopDriver.ino
+++ Simulations/Inverter/Inverter_Arduino/OpenLoopDriver/OpenLoopDriver/OpenLoopDriver.ino
@@ -1,5 +1,4 @@
-//Brushless DC Motor Current Control Drive
-//Arduino Mega2560 
+//Brushless DC Motor Open Loop Drive 
 
 int PhaseaUpper=2;
 int PhaseaLower=3;
@@ -8,15 +7,9 @@ int PhasebLower=5;
 int PhasecUpper=6;
 int PhasecLower=7;
 
-//These pins have attached interrupts 
-int Halla=18
-int Hallb=19
-int Hallc=20
-
-//States of HALL Sensors 
-volatile int HALLA = LOW;
-volatile int HALLB = LOW;
-volatile int HALLC = LOW;
+int Halla=8
+int Hallb=9
+int Hallc=10
 
 void setup()
 {
@@ -38,35 +31,14 @@ digitalWrite(PhasebLower, LOW);
 digitalWrite(PhasecUpper, LOW);
 digitalWrite(PhasecLower, LOW);
 
-HALLA = digitalRead(Halla);
-HALLB = digitalRead(Hallb);
-HALLC = digitalRead(Hallc);
-
-attachInterrupt(digitalPinToInterrupt(Halla), blinka, CHANGE);
-attachInterrupt(digitalPinToInterrupt(Hallb), blinkb, CHANGE);
-attachInterrupt(digitalPinToInterrupt(Hallc), blinkc, CHANGE);
-
 }
 
 void loop()
 {
-/*
 digitalWrite(PhaseaUpper, digitalRead(Halla));
 digitalWrite(PhaseaLower, !digitalRead(Halla));
 digitalWrite(PhasebUpper, digitalRead(Hallb));
 digitalWrite(PhasebLower, !digitalRead(Hallb));
 digitalWrite(PhasecUpper, digitalRead(Hallc));
 digitalWrite(PhasecLower, !digitalRead(Hallc));
-*/
-
-digitalWrite(PhaseaUpper, HALLA);
-digitalWrite(PhaseaLower, !HALLA);
-digitalWrite(PhasebUpper, HALLB);
-digitalWrite(PhasebLower, !HALLB);
-digitalWrite(PhasecUpper, HALLC);
-digitalWrite(PhasecLower, !HALLC);
 }
-
-void blinka() {HALLA = !HALLA;}
-void blinkb() {HALLB = !HALLB;}
-void blinkc() {HALLC = !HALLC;}
