1242ee1b7ba85d039c958eda8da14fd156568d45
Muhammad Shamaas <mshamaas1997@gmail.com>
2020-08-12 23:26:25 +0500

    V1

--- Simulations/Hall_Sensor/Arduino Codes/hall_sensor/hall_sensor.ino
+++ Simulations/Hall_Sensor/Arduino Codes/hall_sensor/hall_sensor.ino
@@ -1,20 +1,55 @@
-int frequency = 1000; //Set frequency in Hertz
+double frequency = 2500.0; //Set frequency in Hertz
 
-double delayTime = 1000 / (frequency * 2);
+double delayTime = 1000000.0 / (frequency);
 
 void setup()
 {
 pinMode(2, OUTPUT);  
 pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);  
+pinMode(5, OUTPUT);
+pinMode(6, OUTPUT);  
+pinMode(7, OUTPUT);
+
 }
 
 void loop()
 {
- digitalWrite(2, LOW);
- digitalWrite(3, HIGH);
- delay(delayTime);
-
  digitalWrite(2, HIGH);
  digitalWrite(3, LOW);
- delay(delayTime);
+ digitalWrite(4, HIGH);
+ digitalWrite(5, LOW);
+ digitalWrite(6, HIGH);
+ digitalWrite(7, LOW);
+
+ delayMicroseconds(delayTime/4.0);
+
+ digitalWrite(2, LOW);
+ digitalWrite(3, LOW);
+ digitalWrite(4, LOW);
+ digitalWrite(5, LOW);
+ digitalWrite(6, LOW);
+ digitalWrite(7, LOW);
+
+ delayMicroseconds(delayTime/4.0);
+
+ 
+ digitalWrite(2, LOW);
+ digitalWrite(3, HIGH);
+ digitalWrite(4, LOW);
+ digitalWrite(5, HIGH);
+ digitalWrite(6, LOW);
+ digitalWrite(7, HIGH);
+
+ delayMicroseconds(delayTime/4.0);
+
+ digitalWrite(2, LOW);
+ digitalWrite(3, LOW);
+ digitalWrite(4, LOW);
+ digitalWrite(5, LOW);
+ digitalWrite(6, LOW);
+ digitalWrite(7, LOW);
+
+ delayMicroseconds(delayTime/4.0);
+
 }
