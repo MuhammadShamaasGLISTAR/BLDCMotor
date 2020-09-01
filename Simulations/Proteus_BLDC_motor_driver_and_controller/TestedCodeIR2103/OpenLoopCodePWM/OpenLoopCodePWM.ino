6fa2a3ec168ecb0776181cd67e13bd87966915f7
Muhammad Shamaas <mshamaas1997@gmail.com>
2020-09-01 15:07:08 +0500

    V1

--- Simulations/Proteus_BLDC_motor_driver_and_controller/TestedCodeIR2103/OpenLoopCodePWM/OpenLoopCodePWM.ino
+++ Simulations/Proteus_BLDC_motor_driver_and_controller/TestedCodeIR2103/OpenLoopCodePWM/OpenLoopCodePWM.ino
@@ -1,3 +1,17 @@
+/*
+Arduino Mega 2560
+Brushless DC Motor Controller using IR2103
+Phase a/b/c Upper Pins can only be connected on pins 2 - 13 or 44 - 46 for analogWrite(pin,dutyCycle) where 0<=dutyCycle<=255
+Phase a/b/c Lower Pins can only be connected on digital pins. The logic for these pins must be inverted for IR2103 to work properly.
+Hall a/b/c Pins can only be connected on digital pins. 
+The HALL a/b/c logic bits are combined as newstate=(HALLC<<2)|(HALLB<<1)|(HALLA) e.g. HALLC=1, HALLB=0, HALLA=1 => newstate=(1<<2)|(0<<1)|1=101 
+Speed Reference Pin is connected on analog pin A0 with ADC. The analog input ranges from 0-5 V. The ADC output ranges from 0-1023
+The Motor runs in forward direction if (ADC_output/2)>255 and dutyCycle=ADC_output-255
+The Motor runs in reverse direction if (ADC_output/2)<255 and dutyCycle=255-ADC_output 
+MOSFETS a/b/c StatusInputPins can only be connected on digital pins. They are used to check switching of a/b/c inverter phases respectively
+MOSFETS a/b/c StatusOutputPins can only be connected on digital pins. They are used to output status of a/b/c inverter phases respectively
+*/
+
 int PhaseaLowerPin=26;
 int PhaseaUpperPin=44;
 int PhasebLowerPin=4;
@@ -7,6 +21,12 @@ int PhasecUpperPin=11;
 int HallaPin=48;
 int HallbPin=50;
 int HallcPin=52;
+int MOSFETSaStatusInputPin=28;
+int MOSFETSaStatusOutputPin=30;
+int MOSFETSbStatusInputPin=3;
+int MOSFETSbStatusOutputPin=2;
+int MOSFETScStatusInputPin=9;
+int MOSFETScStatusOutputPin=8;
 /*int IaPin=A0;
 int IbPin=A1;
 int IcPin=A2;
@@ -14,6 +34,7 @@ int VaPin=A3;
 int VbPin=A4;
 int VcPin=A5;
 */
+
 int HALLA=LOW;
 int HALLB=LOW;
 int HALLC=LOW;
@@ -23,15 +44,7 @@ int PhasebLower=LOW;
 int PhasebUpper=LOW;
 int PhasecLower=LOW;
 int PhasecUpper=LOW;
-
 int oldstate, newstate;
-
-int MOSFETSaStatusInputPin=28;
-int MOSFETSaStatusOutputPin=30;
-int MOSFETSbStatusInputPin=3;
-int MOSFETSbStatusOutputPin=2;
-int MOSFETScStatusInputPin=9;
-int MOSFETScStatusOutputPin=8;
 int Shutdowna=0;
 int Shutdownb=0;
 int Shutdownc=0;
@@ -125,76 +138,44 @@ void loop()
   HALLC=digitalRead(HallcPin);
   oldstate=newstate;  
   newstate=(HALLC<<2)|(HALLB<<1)|(HALLA);
-  
+  //Serial.println(newstate);
 
-  val = analogRead(A0);
+  val = analogRead(A0);//0-1023
+  val=val/2;//0-511;
+  if(val>255){dutyCycle=val-255;Direction=1;}
+  if(val==255){dutyCycle=0;}
+  if(val<255){dutyCycle=255-val;Direction=-1;}
+  //if(dutyCycle>220){dutyCycle=220;}
   //Serial.println(val/4);
-  
-  if(newstate==1){
+
     PhaseaLower=LOW;
-    PhaseaUpper=HIGH;
+    PhaseaUpper=LOW;
     PhasebLower=LOW;
     PhasebUpper=LOW;
-    PhasecLower=HIGH;
+    PhasecLower=LOW;
     PhasecUpper=LOW;
-    }
     
-    if(newstate==2){
-    PhaseaLower=HIGH;
-    PhaseaUpper=LOW;
-    PhasebLower=LOW;
-    PhasebUpper=HIGH;
-    PhasecLower=LOW;
-    PhasecUpper=LOW;
-    }
+  if(Direction==1){
+  if(newstate==1){PhaseaUpper=HIGH;PhasecLower=HIGH;}
+  if(newstate==2){PhaseaLower=HIGH;PhasebUpper=HIGH;}
+  if(newstate==3){PhasebUpper=HIGH;PhasecLower=HIGH;}
+  if(newstate==4){PhasebLower=HIGH;PhasecUpper=HIGH;}
+  if(newstate==5){PhaseaUpper=HIGH;PhasebLower=HIGH;}
+  if(newstate==6){PhaseaLower=HIGH;PhasecUpper=HIGH;}
+  }
 
-    if(newstate==3){
-    PhaseaLower=LOW;
-    PhaseaUpper=LOW;
-    PhasebLower=LOW;
-    PhasebUpper=HIGH;
-    PhasecLower=HIGH;
-    PhasecUpper=LOW;
-    }
+  if(Direction==(-1)){
+  if(newstate==1){PhaseaLower=HIGH;PhasecUpper=HIGH;}
+  if(newstate==2){PhaseaUpper=HIGH;PhasebLower=HIGH;}
+  if(newstate==3){PhasebLower=HIGH;PhasecUpper=HIGH;}
+  if(newstate==4){PhasebUpper=HIGH;PhasecLower=HIGH;}
+  if(newstate==5){PhaseaLower=HIGH;PhasebUpper=HIGH;}
+  if(newstate==6){PhaseaUpper=HIGH;PhasecLower=HIGH;}
+  }
 
-    if(newstate==4){
-    PhaseaLower=LOW;
-    PhaseaUpper=LOW;
-    PhasebLower=HIGH;
-    PhasebUpper=LOW;
-    PhasecLower=LOW;
-    PhasecUpper=HIGH;
-    }
-
-    if(newstate==5){
-    PhaseaLower=LOW;
-    PhaseaUpper=HIGH;
-    PhasebLower=HIGH;
-    PhasebUpper=LOW;
-    PhasecLower=LOW;
-    PhasecUpper=LOW;
-    }
-
-    if(newstate==6){
-    PhaseaLower=HIGH;
-    PhaseaUpper=LOW;
-    PhasebLower=LOW;
-    PhasebUpper=LOW;
-    PhasecLower=LOW;
-    PhasecUpper=HIGH;
-    }
   
-  
-  if(newstate!=oldstate)
+  if(((newstate==6)&&(oldstate==4))||((newstate==4)&&(oldstate==6)))
   {
-/*  digitalWrite(PhaseaLowerPin, !LOW);
-  digitalWrite(PhaseaUpperPin, LOW);
-  digitalWrite(PhasebLowerPin, !LOW);
-  digitalWrite(PhasebUpperPin, LOW);
-  digitalWrite(PhasecLowerPin, !LOW);
-  digitalWrite(PhasecUpperPin, LOW);
-  delayMicroseconds(1);
-  */
   checkMOSFETsStatus();
   }
 
@@ -202,11 +183,11 @@ void loop()
 
  
  digitalWrite(PhaseaLowerPin, !PhaseaLower);
- if(PhaseaUpper){analogWrite(PhaseaUpperPin, val/4);}else{digitalWrite(PhaseaUpperPin, LOW);}
+ if(PhaseaUpper){analogWrite(PhaseaUpperPin, dutyCycle);}else{digitalWrite(PhaseaUpperPin, LOW);}
  digitalWrite(PhasebLowerPin, !PhasebLower);
- if(PhasebUpper){analogWrite(PhasebUpperPin, val/4);}else{digitalWrite(PhasebUpperPin, LOW);}
+ if(PhasebUpper){analogWrite(PhasebUpperPin, dutyCycle);}else{digitalWrite(PhasebUpperPin, LOW);}
  digitalWrite(PhasecLowerPin, !PhasecLower);
- if(PhasecUpper){analogWrite(PhasecUpperPin, val/4);}else{digitalWrite(PhasecUpperPin, LOW);}
+ if(PhasecUpper){analogWrite(PhasecUpperPin, dutyCycle);}else{digitalWrite(PhasecUpperPin, LOW);}
   
  }
   
@@ -275,6 +256,7 @@ void loop()
 
 int checkMOSFETsStatus()
 {
+
   Shutdowna=0;
   Shutdownb=0;
   Shutdownc=0;
@@ -286,29 +268,13 @@ int checkMOSFETsStatus()
   digitalWrite(PhasecUpperPin, LOW);
   delayMicroseconds(1);
   
-  //if(digitalRead(MOSFETSaStatusInputPin)){Shutdowna=1;}
-  //if(digitalRead(MOSFETSbStatusInputPin)){Shutdownb=1;}
-  //if(digitalRead(MOSFETScStatusInputPin)){Shutdownc=1;}
-
   digitalWrite(PhaseaLowerPin,!HIGH);
   digitalWrite(PhasebLowerPin,!HIGH);
   digitalWrite(PhasecLowerPin,!HIGH);
   delayMicroseconds(100);
-  if(digitalRead(MOSFETSaStatusInputPin))
-  {
-    Shutdowna=1;
-  //Serial.println("A LOW Failure");
-  }
-  if(digitalRead(MOSFETSbStatusInputPin))
-  {
-    Shutdownb=1;
-  //Serial.println("B LOW Failure");
-  }
-  if(digitalRead(MOSFETScStatusInputPin))
-  {
-    Shutdownc=1;
-    //Serial.println("C LOW Failure");
-  }
+  if(digitalRead(MOSFETSaStatusInputPin)){Shutdowna=1;}//Serial.println("A LOW Failure");
+  if(digitalRead(MOSFETSbStatusInputPin)){Shutdownb=1;}//Serial.println("B LOW Failure");
+  if(digitalRead(MOSFETScStatusInputPin)){Shutdownc=1;}//Serial.println("C LOW Failure");
 
   digitalWrite(PhaseaLowerPin, !LOW);
   digitalWrite(PhaseaUpperPin, LOW);
@@ -322,23 +288,10 @@ int checkMOSFETsStatus()
   digitalWrite(PhasebUpperPin,HIGH);
   digitalWrite(PhasecUpperPin,HIGH);
   delayMicroseconds(100);
-  if(!digitalRead(MOSFETSaStatusInputPin))
-  {
-    Shutdowna=1;
-    //Serial.println("A HIGH Failure");
-  }
-
-  if(!digitalRead(MOSFETSbStatusInputPin))
-  {
-    Shutdownb=1;
-    //Serial.println("B HIGH Failure");
-  }
-
-  if(!digitalRead(MOSFETScStatusInputPin))
-  {
-    Shutdownc=1;
-    //Serial.println("C HIGH Failure");
-  }
+  
+  if(!digitalRead(MOSFETSaStatusInputPin)){Shutdowna=1;}//Serial.println("A HIGH Failure");
+  if(!digitalRead(MOSFETSbStatusInputPin)){Shutdownb=1;}//Serial.println("B HIGH Failure");
+  if(!digitalRead(MOSFETScStatusInputPin)){Shutdownc=1;}//Serial.println("C HIGH Failure");
 
   if(Shutdowna==1){digitalWrite(MOSFETSaStatusOutputPin,LOW);}else{digitalWrite(MOSFETSaStatusOutputPin,HIGH);}
   if(Shutdownb==1){digitalWrite(MOSFETSbStatusOutputPin,LOW);}else{digitalWrite(MOSFETSbStatusOutputPin,HIGH);}
