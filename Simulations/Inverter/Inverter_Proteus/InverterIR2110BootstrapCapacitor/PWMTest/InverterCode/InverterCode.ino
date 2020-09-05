d25055937dd9195a0091484247096f8952a603cf
Muhammad Shamaas <mshamaas1997@gmail.com>
2020-09-05 16:37:31 +0500

    V1

--- Simulations/Inverter/Inverter_Proteus/InverterIR2110BootstrapCapacitor/PWMTest/InverterCode/InverterCode.ino
+++ Simulations/Inverter/Inverter_Proteus/InverterIR2110BootstrapCapacitor/PWMTest/InverterCode/InverterCode.ino
@@ -1,12 +1,24 @@
-double frequency = 1000.0; //Set frequency in Hertz
+#include <PWM.h>
+
  
-double delayTime = 1000000.0 / (frequency);
+//double delayTime = 1000000.0 / (frequency);
 
 unsigned int dutyCycle=500;
 
+int32_t frequency = 50; //frequency (in Hz)
+
+
 void setup()
 {
-  
+ pinMode(6, OUTPUT);
+   
+    InitTimersSafe(); 
+
+  bool success = SetPinFrequencySafe(3, frequency);
+ if(success) {
+    digitalWrite(6, HIGH);    
+  }
+
 pinMode(2, OUTPUT);  
 pinMode(3, OUTPUT);
 pinMode(4, INPUT);  
@@ -27,7 +39,9 @@ void loop()
  if(!digitalRead(4)){digitalWrite(5,HIGH);}else{digitalWrite(5,LOW);}
  
  digitalWrite(2, LOW);
- analogWrite(3, 1);
+ //analogWrite(3, 1);
+   pwmWrite(3, 200);
+
  delay(1000);
  
  
